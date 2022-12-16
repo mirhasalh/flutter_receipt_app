d090da9fb4fd0c0ac4d0bc19df5cf31e5711532f
Irhas <muhammadirhasalh@gmail.com>
2022-12-16 10:32:59 +0700

    Add palette.dart

---
+++ lib/src/palette.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/material.dart';
+
+class Palette {
+  static const maximumYellowRed = Color(0xfff4b432);
+  static const naplesYellow = Color(0xfffde168);
+  static const alabaster = Color(0xffedeade);
+}
