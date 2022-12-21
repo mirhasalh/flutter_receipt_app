5edb47f331a2885cd0ec48230e6811a473b55855
Irhas <muhammadirhasalh@gmail.com>
2022-12-21 18:56:16 +0700

    Remove unused comments

--- lib/main.dart
+++ lib/main.dart
@@ -3,12 +3,5 @@ import 'package:flutter_receipt_app/src/app.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 void main() {
-  // WidgetsFlutterBinding.ensureInitialized();
-
-  // SystemChrome.setPreferredOrientations([
-  //   DeviceOrientation.portraitUp,
-  //   DeviceOrientation.portraitDown,
-  // ]);
-
   runApp(const ProviderScope(child: App()));
 }
