7c382b5dc2b6dce3a11927435495396e4bd2eea6
Irhas <muhammadirhasalh@gmail.com>
2022-12-21 15:52:52 +0700

    Update home.dart

--- lib/src/page/home.dart
+++ lib/src/page/home.dart
@@ -1,5 +1,6 @@
 import 'package:flutter_receipt_app/src/common.dart';
 import 'package:flutter_receipt_app/src/page/pages.dart';
+import 'package:flutter_receipt_app/src/palette.dart';
 import 'package:flutter_speed_dial/flutter_speed_dial.dart';
 
 class Home extends StatelessWidget {
@@ -15,10 +16,14 @@ class Home extends StatelessWidget {
       ),
       floatingActionButton: SpeedDial(
         tooltip: AppLocalizations.of(context)!.openMenu,
+        backgroundColor: Palette.naplesYellow,
+        foregroundColor: Palette.eerieBlack,
         children: [
           SpeedDialChild(
             onTap: () {},
             label: AppLocalizations.of(context)!.createNewDoc,
+            labelStyle: const TextStyle(color: Palette.eerieBlack),
+            foregroundColor: Palette.eerieBlack,
             child: const Icon(Icons.add),
           ),
           SpeedDialChild(
@@ -26,6 +31,8 @@ class Home extends StatelessWidget {
               Navigator.of(context).pushNamed(ItemList.routeName);
             },
             label: AppLocalizations.of(context)!.seeAllItems,
+            labelStyle: const TextStyle(color: Palette.eerieBlack),
+            foregroundColor: Palette.eerieBlack,
             child: const Icon(Icons.all_inbox_outlined),
           ),
           SpeedDialChild(
@@ -33,6 +40,8 @@ class Home extends StatelessWidget {
               Navigator.of(context).pushNamed(SupplierList.routeName);
             },
             label: AppLocalizations.of(context)!.supplier,
+            labelStyle: const TextStyle(color: Palette.eerieBlack),
+            foregroundColor: Palette.eerieBlack,
             child: const Icon(Icons.warehouse_outlined),
           ),
           SpeedDialChild(
@@ -40,6 +49,8 @@ class Home extends StatelessWidget {
               Navigator.of(context).pushNamed(Settings.routeName);
             },
             label: AppLocalizations.of(context)!.settings,
+            labelStyle: const TextStyle(color: Palette.eerieBlack),
+            foregroundColor: Palette.eerieBlack,
             child: const Icon(Icons.settings_outlined),
           ),
         ],
