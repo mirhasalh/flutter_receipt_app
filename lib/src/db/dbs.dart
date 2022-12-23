e74062af6a325a003cd2f4b495b53ee74a1334b1
Irhas <muhammadirhasalh@gmail.com>
2022-12-23 18:32:15 +0700

    Update db utils path

--- lib/src/db/dbs.dart
+++ lib/src/db/dbs.dart
@@ -1,4 +1,4 @@
-export 'package:flutter_receipt_app/src/db/db_utils.dart';
+export 'package:flutter_receipt_app/src/utils/db_utils.dart';
 export 'package:flutter_receipt_app/src/db/item.dart';
 export 'package:flutter_receipt_app/src/db/supplier.dart';
 export 'package:flutter_receipt_app/src/db/txn_document.dart';
