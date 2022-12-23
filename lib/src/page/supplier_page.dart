5ec66d87613330f2bccf6e89a1228b5c7c6107cc
Irhas <muhammadirhasalh@gmail.com>
2022-12-23 16:15:23 +0700

    Remove unused import

--- lib/src/page/supplier_page.dart
+++ lib/src/page/supplier_page.dart
@@ -1,4 +1,3 @@
-import 'package:flutter/gestures.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter_receipt_app/src/common.dart';
 import 'package:flutter_receipt_app/src/constant/sizes.dart';
