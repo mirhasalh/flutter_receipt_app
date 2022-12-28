7214e8ebebe1577348bcde9be94d3639f1e66a01
Irhas <muhammadirhasalh@gmail.com>
2022-12-28 17:41:41 +0700

    Update theme.dart

--- lib/src/theme/theme.dart
+++ lib/src/theme/theme.dart
@@ -29,6 +29,7 @@ ThemeData kTheme = ThemeData(
     titleTextStyle: TextStyle(
       color: Palette.eerieBlack,
       fontSize: 20.0,
+      fontWeight: FontWeight.bold,
     ),
     elevation: 0.0,
   ),
