f627f2eb8b8f405722146ee8a748fff0e9d46139
Irhas <muhammadirhasalh@gmail.com>
2022-12-29 22:07:56 +0700

    Update palette.dart

--- lib/src/palette.dart
+++ lib/src/palette.dart
@@ -9,4 +9,6 @@ class Palette {
   static const azure = Color(0xffeafbf6);
   static const floralWhite = Color(0xfffffcf4);
   static const goldFusion = Color(0xff8b703d);
+  static const gunmetal = Color(0xff223040);
+  static const raisinBlack = Color(0xff1d242f);
 }
